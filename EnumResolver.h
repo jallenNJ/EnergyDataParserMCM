859531139ae8ee58d7ad391f3a97cb338b090c4a
Joseph Allen <33922179+jallenNJ@users.noreply.github.com>
2018-03-09 21:29:24 -0500

    Optimized enum resolution
    Removed the if else block from hell with a use of a map to resolve the conversions.

--- EnumResolver.h
+++ EnumResolver.h
@@ -3,12 +3,20 @@
 #include "Enums.h"
 #include <string>
 #include <iostream>
+#include <map>
+#include <fstream>
+#include <sstream>
+
+//Typedef the iterators to something readable
+typedef std::_Tree_iterator<std::_Tree_val<std::_Tree_simple_types<std::pair<std::basic_string<char, std::char_traits<char>, std::allocator<char>>const, int>>>> stringToEnumIterator;
+typedef std::_Tree_iterator<std::_Tree_val<std::_Tree_simple_types<std::pair<int const, std::basic_string<char, std::char_traits<char>, std::allocator<char>>>>>> enumToStringIterator;
+
 
 //This class allows the code to access convertions between enum and natural input
 class EnumResolver {
 public:
 	EnumResolver(){
-
+		intializeMap();
 	}
 
 	int codeNameToEnum(std::string);
@@ -17,6 +25,10 @@ public:
 	std::string codeEnumToName(int);
 
 private:
+
+	void intializeMap();
+	std::map<std::string, int> stringToEnumMap;
+	std::map<int, std::string> enumToStringMap;
 	
 };
 
